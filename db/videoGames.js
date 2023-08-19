b5d585fec5031434666b0ea92ed0db4d5ccebe45
hadafasa <hadafasa@DESKTOP-C7RN5OR>
2023-08-18 21:50:41 -0500

    updated

--- db/videoGames.js
+++ db/videoGames.js
@@ -6,10 +6,10 @@ const REPLACE_ME = 'HELP REPLACE ME!!!!';
 // GET - /api/video-games - get all video games
 async function getAllVideoGames() {
     try {
-        const { rows: videoGames } = await client.query(REPLACE_ME);
+        const { rows: videoGames } = await client.query('SELECT * FROM videoGames');
         return videoGames;
     } catch (error) {
-        throw new Error("Make sure you have replaced the REPLACE_ME placeholder.")
+        throw error;
     }
 }
 
@@ -28,17 +28,47 @@ async function getVideoGameById(id) {
 
 // POST - /api/video-games - create a new video game
 async function createVideoGame(body) {
-    // LOGIC GOES HERE
+    try {
+        const { title, genre } = body;
+        const { rows: [videoGame] } = await client.query(`
+            INSERT INTO videoGames (title, genre) 
+            VALUES ($1, $2) 
+            RETURNING *;
+        `, [title, genre]);
+        
+        return videoGame;
+    } catch (error) {
+        throw error;
+    }
 }
 
 // PUT - /api/video-games/:id - update a single video game by id
 async function updateVideoGame(id, fields = {}) {
-    // LOGIC GOES HERE
+    const setString = Object.keys(fields).map((key, index) => `${key}=$${index + 1}`).join(', ');
+    try {
+        const { rows: [videoGame] } = await client.query(`
+            UPDATE videoGames 
+            SET ${setString} 
+            WHERE id = $${Object.keys(fields).length + 1} 
+            RETURNING *;
+        `, [...Object.values(fields), id]);
+        
+        return videoGame;
+    } catch (error) {
+        throw error;
+    }
 }
 
 // DELETE - /api/video-games/:id - delete a single video game by id
 async function deleteVideoGame(id) {
-    // LOGIC GOES HERE
+    try {
+        await client.query(`
+            DELETE FROM videoGames 
+            WHERE id = $1;
+        `, [id]);
+    } catch (error) {
+        throw error;
+    }
 }
 
 module.exports = {
